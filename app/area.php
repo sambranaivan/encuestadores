cd3527fb443296018e4af37b368e254eb9cb51fa
Sambrana Ivan <sambranaivan@gmail.com>
2019-04-30 10:59:24 -0300

    fix efectivo

--- app/area.php
+++ app/area.php
@@ -7,4 +7,40 @@ use Illuminate\Database\Eloquent\Model;
 class area extends Model
 {
     //
+    public function encuestas()
+    {
+        return $this->hasMany('App\encuesta');
+    }
+
+    public function getEfectiva()
+    {
+        $c = 0;
+
+        $e = $this->encuestas;
+        foreach ($e as $key => $value) {
+
+            if($value->efectivo)
+            {
+                $c++;
+            }
+        }
+
+        return $c;
+    }
+
+      public function getNoEfectiva()
+    {
+        $c = 0;
+
+        $e = $this->encuestas;
+        foreach ($e as $key => $value) {
+
+            if(!$value->efectivo)
+            {
+                $c++;
+            }
+        }
+
+        return $c;
+    }
 }

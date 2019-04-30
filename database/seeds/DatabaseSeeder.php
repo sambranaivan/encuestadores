5efbac29aad462972b5e5c853262d46fe1aae1c7
Sambrana Ivan <sambranaivan@gmail.com>
2019-04-30 06:59:21 -0300

    update seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,29 +16,76 @@ class DatabaseSeeder extends Seeder
     }
     public function userDefecto(){
         $u = new User();
-        $u->name = 'ivan-admin';
-        $u->email = 'direccion';
+        $u->name = 'Francisco';
+        $u->email = 'bosco';
         $u->role = 3;
+        $u->password = bcrypt('1234');
+        $u->save();
+        $u = new User();
+        $u->name = 'Martin Micelli';
+        $u->email = 'martin';
+        $u->role = 3;
+        $u->password = bcrypt('1234');
+        $u->save();
+        $u = new User();
+        $u->name = 'Paula Salvay';
+        $u->email = 'paula';
+        $u->role = 3;
+        $u->password = bcrypt('1234');
+        $u->save();
+        $u = new User();
+        $u->name = 'Ivan Sambrana';
+        $u->email = 'ivan';
+        $u->role = 3;
+        $u->password = bcrypt('1234');
+        $u->save();
+        // Coordinadores
+        $u = new User();
+        $u->name = 'Mabel';
+        $u->email = 'mabel';
+        $u->role = 2;
+        $u->password = bcrypt('1234');
+        $u->save();
+
+        // Supervisores
+        $u = new User();
+        $u->name = 'Carlos Pavon';
+        $u->email = 'pavon';
+        $u->role = 1;
         $u->password = bcrypt('1234');
         $u->save();
 
         $u = new User();
-        $u->name = 'ivan-coordinador';
-        $u->email = 'coordinador';
-        $u->role = 2;
-        $u->password = bcrypt('1234');
-        $u->save();
-        $u = new User();
-        $u->name = 'ivan-supervisor';
-        $u->email = 'supervisor';
+        $u->name = 'Eduardo Bravo';
+        $u->email = 'eduardo';
         $u->role = 1;
         $u->password = bcrypt('1234');
         $u->save();
+        // encuestadores / recuperadores
         $u = new User();
-        $u->name = 'ivan-encuestador';
-        $u->email = 'encuestador';
+        $u->name = 'Carla';
+        $u->email = 'carla';
         $u->role = 0;
         $u->password = bcrypt('1234');
         $u->save();
-        }
-}
+        $u = new User();
+        $u->name = 'Sheila';
+        $u->email = 'sheila';
+        $u->role = 0;
+        $u->password = bcrypt('1234');
+        $u->save();
+        $u = new User();
+        $u->name = 'Vanesa';
+        $u->email = 'vanesa';
+        $u->role = 0;
+        $u->password = bcrypt('1234');
+        $u->save();
+        $u = new User();
+        $u->name = 'Mariana';
+        $u->email = 'mariana';
+        $u->role = 0;
+        $u->password = bcrypt('1234');
+        $u->save();
+    }
+
+    }
